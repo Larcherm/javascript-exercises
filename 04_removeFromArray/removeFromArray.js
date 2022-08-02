7da922489a9f3d8ef792eb8c37a6459b72ba6374
miguel <larchermgl@gmail.com>
2022-08-02 10:57:51 +0100

    Complete javascript exercises 01 through 07

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,16 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array) {
+    
+    const args = Array.prototype.slice.call(arguments, 1);
+    for (let i = 0; i < array.length; i++) {
+        for (let arg of args) {
+            if (array[i] === arg) {
+                array.splice(i, 1);
+                i--;
+            }
+        }       
+    }
+    return array;
 };
-
+console.log(removeFromArray([1, 2, 4, 3], 4, 2));
 // Do not edit below this line
-module.exports = removeFromArray;
+ module.exports = removeFromArray;
